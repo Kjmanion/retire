85b07a0c45a08f934a5c7ee696e7c42058249867
unknown <kevinjmanion1@gmail.com>
2018-11-23 11:00:12 -0500

    Getting ajax to actually work in this learning project and deleting alot of unnecessary code

--- application/views.py
+++ application/views.py
@@ -8,62 +8,17 @@ import re
 
 @app.route('/', methods=['GET'])
 def home():
-    return redirect(url_for('getStates'))
-
-@app.route('/getStates', methods=['GET', 'POST'])
-def getStates():
     form = StateForm(request.form)
     states = session.query(States).all()
     form.selections.choices = [(state.gid, state.name) for state in states]
     form.description = 'Select a state'
-    if request.method == 'POST':
-        print ('Making a post request!')
-        state_id = form.selections.data
-        state = session.query(States).get(state_id)
-        print (state.name)
-        statement = "SELECT name, ST_AsText(ST_Centroid('{}')) as geom FROM states WHERE states.name = '{}'".format(state.geom, state.name)
-        state2 = session.execute(statement).first()
-        print (state2)
-        print (state2.geom)
-        statement2 = """
-        SELECT row_to_json(fc)
-            FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) as features
-            FROM (SELECT 'Feature' As type
-	            , ST_AsGEOJSON('{}')::json As geometry
-	            , row_to_json((SELECT l FROM (SELECT name) As l
-		        )) As properties
-	          FROM states as lg    ) As f ) As fc;
-        """.format(state.geom)
-        stateGeom = session.execute(statement2)
-        test = []
-        print (stateGeom)
-        geoJSON = ''
-        for item in stateGeom:
-            print (item)
-            geoJSON += str(item)
-            test.append(dict(item))
-        geoJSON2 = str(test[0]['row_to_json'])
-        # leng = len(geoJSON)
-        # test2 = test[[0]'row_to_json']]
-        # geoJSON = geoJSON[1:leng-2]
-        newGeo = geoJSON2.replace("'", '"')
-        
-        
-        result = re.search('\W\d+\W\d+\W\d+\W\d+', state2.geom)
-        latLng = result[0].split(' ')
-        print (latLng)
-
-
-
-
-        
-        return render_template('base.html', form=form, lng=latLng[0], lat=latLng[1], geomJson = newGeo)
-
-
     return render_template('base.html', form=form)
 
-@app.route('/test', methods=['GET'])
+
+@app.route('/test', methods=['GET', 'POST'])
 def test():
-    statement = "SELECT name, ST_AsGeoJson(states.geom) as geom FROM states WHERE states.name = '{}'".format('Maryland')
+    req = request.get_json()
+    print (req)
+    statement = "SELECT name, ST_AsGeoJson(states.geom) as geom, ST_AsGeoJSON(ST_AsText(ST_Centroid(states.geom))) as centroid FROM states WHERE states.name = '{}'".format(req['data'])
     state2 = session.execute(statement).first()
-    return jsonify(result=state2.geom, name=state2.name)
+    return jsonify(result=state2.geom, name=state2.name, center=state2.centroid)
